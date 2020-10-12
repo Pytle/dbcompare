83e162d26b48b29e9355cea35072c190c955929a
pytle <954119377@qq.com>
2020-10-12 19:59:27 +0800

    Signed-off-by: pytle <954119377@qq.com>

--- tasks.py
+++ tasks.py
@@ -86,13 +86,14 @@ def compare(db,table,priname,colunms,pri,log,errlog):
             if a == num:            # 如果到了最后一轮，以防不足MTU值，最后索引就取总长度-1
                 endindex = -1
             else:
-                endindex = (a+1)*MTU -2
+                endindex = (a+1)*MTU - 2
             # 先比较100个
             try:
                 startpri = pri[startindex]
             except Exception as e:
                 print("length:%d" % length)
-                print("endindex:%d" % startindex)            
+                print("startindex:%d" % startindex)
+                
             try:
                 endpri = pri[endindex]
             except Exception as e:
