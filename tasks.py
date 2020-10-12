5c7d8271273107cc5c48c3af79bf5ef2c56944f7
pytle <954119377@qq.com>
2020-10-12 19:36:54 +0800

    Signed-off-by: pytle <954119377@qq.com>

--- tasks.py
+++ tasks.py
@@ -84,9 +84,9 @@ def compare(db,table,priname,colunms,pri,log,errlog):
         for a in range(num + 1) :
             startindex = a * MTU
             if a == num:            # 如果到了最后一轮，以防不足MTU值，最后索引就取总长度-1
-                endindex = len(pri) - 1
+                endindex = -1
             else:
-                endindex = (a+1)*MTU - 1
+                endindex = (a+1)*MTU 
             # 先比较100个
             try:
                 startpri = pri[startindex]
